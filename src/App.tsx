0967bd11f101601c8d73b50e5614f48405186dae
maria-parreira <mariaparreira71@gmail.com>
2024-08-11 13:33:21 +0100

    Introduce more components App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import Header from './components//Header';
-import About from './components//Aboutme';
+import Header from './components/Header';
+import Aboutme from './components/Aboutme';
+import Education from './components/Education';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import Experience from './components/Experience';
+import Resume from './components/Resume';
 import Footer from './components/Footer';
 
 const App: React.FC = () => {
     return (
         <div>
             <Header />
-            <About />
+            <Aboutme />
+            <Education />
             <Skills />
             <Projects />
+            <Experience />
+            <Resume />
             <Footer />
         </div>
     );
