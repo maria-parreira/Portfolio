c294c9b4669cda073647936b31dd96b9123ac608
maria-parreira <mariaparreira71@gmail.com>
2024-08-29 16:18:40 +0100

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteStaticCopy({
-    targets: [
-      {
-        src: 'public/**/*',
-        dest: ''
-      }
-    ]
-  })],
+  plugins: [react()],
   base: '/portfolio/',
 })
