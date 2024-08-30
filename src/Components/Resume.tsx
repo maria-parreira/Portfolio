044f1ba70ad5fbb8fcde39c1156332672bf42d5d
maria-parreira <mariaparreira71@gmail.com>
2024-08-30 16:52:43 +0100

    Update Resume component

--- src/Components/Resume.tsx
+++ src/Components/Resume.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import resume from '../assets/CV.pdf';
+import downloadIcon from '../assets/cvicon.jpg'; // Importe a imagem
 import '../styles/Resume.css';
-import '../styles/SectionAdjustment.css'
+import '../styles/SectionAdjustment.css';
 
 const Resume: React.FC = () => {
     return (
         <section id="Resume">
             <h2>Resume</h2>
             <p>
-                For a detailed overview of my professional background, skills, and experiences, please download my resume using the link below:
+                For a detailed overview of my professional background, skills, and experiences, click below:
             </p>
-            <a href={resume} download="Maria_Parreira_Resume.pdf" className="resume-link">
-                Download Resume
+            <a href={resume} download="Maria_Parreira_CV.pdf" className="resume-link">
+                <img src={downloadIcon} alt="Download CV" className="download-image" />
             </a>
         </section>
     );
