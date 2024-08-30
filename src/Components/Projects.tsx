f735c6f6b14c0a699fc29e24dc4d3d8e0f7d5f8e
maria-parreira <mariaparreira71@gmail.com>
2024-08-30 16:17:46 +0100

    Update Projects component

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,28 +1,48 @@
 import React from 'react';
 import '../styles/Projects.css';
-import '../styles/SectionAdjustment.css'
+import '../styles/SectionAdjustment.css';
 
 interface Project {
     title: string;
-    description: string;
     link: string;
+    image: string;  // Adicionando um campo para a imagem
 }
 
 const projects: Project[] = [
-    { title: '', description: '', link: '' },
-    // Add more projects here
+    {
+        title: 'Smart Home',
+        link: 'https://github.com/maria-parreira/smarthome-switch-dev',
+        image: 'src/assets/shome.png'
+    },
+    {
+        title: 'TCP vs UPD',
+        link: 'https://github.com/maria-parreira/scomred-switch-dev',
+        image: 'src/assets/tcpudp.png'
+    },
+    {
+        title: 'DevOps',
+        link: 'https://github.com/maria-parreira/devops-switch-dev',
+        image: 'src/assets/devops.jpg'
+    },
+    {
+        title: 'Lost and Found',
+        link: 'https://github.com/maria-parreira/lost-and-found',
+        image: 'src/assets/lf.png'
+    },
+
 ];
 
 const Projects: React.FC = () => {
     return (
         <section id="Projects">
             <h2>Projects</h2>
-            <div>
+            <div className="projects-container">
                 {projects.map((project, index) => (
-                    <div key={index}>
+                    <div key={index} className="project-item">
                         <h3>{project.title}</h3>
-                        <p>{project.description}</p>
-                        <a href={project.link}>View Project</a>
+                        <a href={project.link} target="_blank" rel="noopener noreferrer">
+                            <img src={project.image} alt={`${project.title} screenshot`} className="project-image" />
+                        </a>
                     </div>
                 ))}
             </div>
