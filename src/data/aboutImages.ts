7c547325fe13b395c1d7ad1dbe90144d66d2cfa1
maria-parreira <mariaparreira71@gmail.com>
2025-07-13 11:36:26 +0100

    update about section

--- src/data/aboutImages.ts
+++ src/data/aboutImages.ts
@@ -1,8 +1,8 @@
 ﻿// src/data/aboutImages.ts
 
-import img1 from "../Portfolio/profile-image.jpg";
-import img2 from "../Portfolio/about/me-2.jpg";
-import img3 from "../Portfolio/about/me-3.jpg";
-import img4 from "../Portfolio/about/image4.jpg";
+import img1 from "../../public/image4.jpg";
+import img2 from "../../public/me-2.jpg";
+import img3 from "../../public/me-3.jpg";
+import img4 from "../../public/image5.jpg";
 
 export const aboutImages: string[] = [img1, img2, img3, img4];
